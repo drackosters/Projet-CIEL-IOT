c1b14c5678942698f2671f8ff5078f0732caae40
drackosters <vincke.alexis@lyceejulesfil.fr>
2025-04-22 13:05:03 +0200

    Auto-sync: 2025-04-22 13:05:03

--- settings.php
+++ settings.php
@@ -190,7 +190,7 @@
         <h2>Gestion des IoT</h2>
         <div class="boutons-container">
             <button class="bouton-action bouton-creationIoT" 
-                    onmouseover="afficherTexteBas('Cliquez pour <br>ajouter un IoT')" 
+                    onmouseover="afficherTexteBas('Cliquez pour <br><br><br><br><br><br>ajouter un IoT')" 
                     onmouseout="cacherTexteBas()" 
                     onclick="ouvrirIframe('Création d\'un IoT')">Création d'un IoT</button>
             <button class="bouton-action bouton-creationTopic" 
