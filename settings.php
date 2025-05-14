fabeda6888a2a689eea2cb5178cc1975076ecff8
drackosters <vincke.alexis@lyceejulesfil.fr>
2025-05-14 10:50:35 +0200

    Auto-sync: 2025-05-14 10:50:35

--- settings.php
+++ settings.php
@@ -34,21 +34,22 @@ if (
         }
 
         /* Barre du haut */
-        .conteneur-haut {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            width: 100%;
-            padding: 20px 40px;
-            background-color: rgba(255, 255, 255, 0.1);
-            backdrop-filter: blur(10px);
-            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
-            position: fixed;
-            top: 0;
-            left: 0;
-            right: 0;
-            z-index: 1000;
-        }
+    .conteneur-haut {
+        display: flex;
+        justify-content: center; /* Centre le contenu horizontalement */
+        align-items: center;
+        width: 100%;
+        padding: 20px 40px;
+        background-color: rgba(255, 255, 255, 0.1);
+        backdrop-filter: blur(10px);
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        z-index: 1000;
+    }
+
 
         .logo {
             height: 50px;
