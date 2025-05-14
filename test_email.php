c4c8685c63ee1a86576f88d844b800271edb0394
drackosters <vincke.alexis@lyceejulesfil.fr>
2025-05-14 12:45:02 +0200

    Auto-sync: 2025-05-14 12:45:02

--- test_email.php
+++ test_email.php
@@ -1,12 +1,26 @@
 <?php
-$to = "julienmolina11700@gmail.com";
-$subject = "Test mail PHP";
-$message = "Ceci est un test depuis ton serveur.";
-$headers = "From: test@tonsite.com\r\n";
 
-if (mail($to, $subject, $message, $headers)) {
-    echo "Mail envoyé avec succès.";
-} else {
-    echo "Échec de l'envoi du mail.";
+if (!empty($alertes)) {
+    $sujet = "Alertes IoT détectées";
+    $message = "Bonjour $nom_utilisateur,\n\nVoici les alertes détectées :\n\n" . implode("\n", $alertes) . "\n\nVeuillez vérifier vos équipements.";
+
+    try {
+        $mail = new PHPMailer(true);
+        $mail->isSMTP();
+        $mail->Host = 'smtp.titan.email';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'alerte@plagiot.tech';
+        $mail->Password = 'Pl@gI0T-@lert3';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        $mail->setFrom('alerte@plagiot.tech', 'Système IoT');
+        $mail->addAddress($email);
+        $mail->Subject = $sujet;
+        $mail->Body = $message;
+        $mail->isHTML(false);
+        $mail->send();
+    } catch (Exception $e) {
+        error_log("Erreur envoi email : " . $mail->ErrorInfo);
+    }
 }
-?>
