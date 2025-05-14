09b50bb40c482a0155be56eca82b459637e3ebcd
drackosters <vincke.alexis@lyceejulesfil.fr>
2025-05-14 10:14:04 +0200

    Auto-sync: 2025-05-14 10:14:04

--- Connexion.php
+++ Connexion.php
@@ -1,4 +1,3 @@
-
 <?php
 session_start();
 
@@ -16,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['connexion'])) {
         $stmt->execute([$login]);
         $utilisateur = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        if ($utilisateur && password_verify($mot_de_passe, $utilisateur['mots_de_passe'])) {
+        if ($utilisateur && $utilisateur['mots_de_passe'] == $mot_de_passe) {
             $_SESSION['utilisateur_connecte'] = true;
             $_SESSION['login_admin'] = $utilisateur['Nom'];
             $_SESSION['Utilisateur'] = $utilisateur['nom'];
@@ -66,7 +65,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['connexion'])) {
 
         <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> " style="display: flex; flex-direction: column; width: 300px; margin: auto;">
 
-            <input type="text" name="login" placeholder="Nom d'utilisateur ou adresse e-mail"
+            <input type="text" name="login" placeholder="Nom d'utilisateur"
              required style="padding: 10px; margin-bottom: 15px; border: 1px solid #ccc; border-radius: 5px;">
 
             <input type="password" name="pswd" placeholder="Mot de passe"
